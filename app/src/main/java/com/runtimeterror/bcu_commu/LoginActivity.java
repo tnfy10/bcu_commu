76d2621498d85e9d92bc56e87df4e39d2ea325e6
tnfy10 <tnfy10@naver.com>
2020-11-02 18:43:39 +0900

    1. 타이틀바 제거 2. 로그인 화면에서 회원가입 버튼 누를 시 회원가입 화면 전환 기능 추가

---
+++ app/src/main/java/com/runtimeterror/bcu_commu/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.runtimeterror.bcu_commu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    Button loginBtn;
+    Button registerBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+    }
+}
