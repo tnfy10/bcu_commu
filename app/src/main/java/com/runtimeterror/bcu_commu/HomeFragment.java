75c9f1a66311a53e7385db46cffd82c60b11bcd3
tnfy10 <tnfy10@naver.com>
2020-11-11 20:16:19 +0900

    각 게시판 레이아웃 추가 홈화면 게시판 더보기 버튼을 누를 시 해당 게시판으로 이동하도록 코드 추가 설정화면 앱 버전에 버전이 몇인지 나오도록 코드 추가

--- app/src/main/java/com/runtimeterror/bcu_commu/HomeFragment.java
+++ app/src/main/java/com/runtimeterror/bcu_commu/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.runtimeterror.bcu_commu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
@@ -12,10 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
+    Button freeMoreBtn;
+    Button subjMoreBtn;
+    Button meetMoreBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup homeView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        final ViewGroup homeView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
         RecyclerView redirectView = homeView.findViewById(R.id.redirectView);
 
@@ -33,6 +38,33 @@ public class HomeFragment extends Fragment {
         adapter.addItem(new Site("사이트8"));
 
         redirectView.setAdapter(adapter);
+
+        freeMoreBtn = homeView.findViewById(R.id.freeMoreBtn);
+        freeMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent freeIntent = new Intent(homeView.getContext(), Freeboard.class);
+                startActivity(freeIntent);
+            }
+        });
+
+        subjMoreBtn = homeView.findViewById(R.id.subjMoreBtn);
+        subjMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent subjIntent = new Intent(homeView.getContext(), Subjboard.class);
+                startActivity(subjIntent);
+            }
+        });
+
+        meetMoreBtn = homeView.findViewById(R.id.meetMoreBtn);
+        meetMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent meetIntent = new Intent(homeView.getContext(), Meetboard.class);
+                startActivity(meetIntent);
+            }
+        });
 
         return homeView;
     }
--- app/src/main/java/com/runtimeterror/bcu_commu/SetFragment.java
+++ app/src/main/java/com/runtimeterror/bcu_commu/SetFragment.java
@@ -1,17 +1,49 @@
 package com.runtimeterror.bcu_commu;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class SetFragment extends Fragment {
+    TextView txtVer;
+    TextView userId;
+    TextView userName;
+    TextView userStdNum;
+
+    Button imgChgBtn;
+    Button PWChgBtn;
+    Button QnABtn;
+    Button appNoticeBtn;
+    Button privacyInfoBtn;
+    Button quitBtn;
+    Button logoutBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_set, container, false);
+        final ViewGroup setView = (ViewGroup) inflater.inflate(R.layout.fragment_set, container, false);
+
+        txtVer = setView.findViewById(R.id.txtVer);
+        txtVer.setText(getVersionInfo(setView.getContext()));
+
+        return setView;
+    }
+
+    // 앱 버전 반환
+    public String getVersionInfo(Context context){
+        String version = null;
+        try{
+            PackageInfo pi = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            version = pi.versionName;
+        }catch(PackageManager.NameNotFoundException e) {}
+        return version;
     }
 
 }
