86bbdf9fb432b6552fc03df8c82a84b26903643a
cookie3029 <whitewalls@naver.com>
2020-12-03 08:47:27 +0900

    일정 삭제 기능 구현
    // 삭제를 완료한 직후 일정으로 돌아오면 일정이 지워지지 않는 이상 발생
    
    // DB에는 무사히 지워진 상태로 화면을 다른 곳으로 넘겼다가 돌아오면 지워지긴 하는 것이 확인됨
    
    // 이외에도 안드로이드 콘솔창에 json 에러 문구가 출력되면서 일정이 2개가 생기는 경우가 발생됨

--- app/src/main/java/com/runtimeterror/bcu_commu/DetailSchdActivity.java
+++ app/src/main/java/com/runtimeterror/bcu_commu/DetailSchdActivity.java
@@ -1,9 +1,12 @@
 package com.runtimeterror.bcu_commu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +14,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class DetailSchdActivity extends AppCompatActivity {
     ImageView deleteBtn;
@@ -55,7 +59,10 @@ public class DetailSchdActivity extends AppCompatActivity {
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO - 일정 삭제
+                deleteAlert(v);
+                // 삭제를 완료한 직후 일정으로 돌아오면 일정이 지워지지 않는 이상 발생
+                // DB에는 무사히 지워진 상태로 화면을 다른 곳으로 넘겼다가 돌아오면 지워지긴 하는 것이 확인됨
+                // 이외에도 안드로이드 콘솔창에 json 에러 문구가 출력되면서 일정이 2개가 생기는 경우가 발생됨
             }
         });
 
@@ -80,5 +87,36 @@ public class DetailSchdActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finish();
+    }
+
+    public void deleteAlert(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setTitle("회원탈퇴");
+        builder.setMessage("정말로 삭제 하시겠습니까?");
+        builder.setPositiveButton("확인",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            DeleteSchd task = new DeleteSchd();
+                            String result = task.execute(user_id, schdNum).get();
+                            Boolean check = Boolean.parseBoolean(result);
+                            Log.d("check", check.toString());
+                            if(check) {
+                                finish();
+                            }
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        builder.show();
     }
 }
--- app/src/main/java/com/runtimeterror/bcu_commu/IpAddress.java
+++ app/src/main/java/com/runtimeterror/bcu_commu/IpAddress.java
@@ -1,5 +1,5 @@
 package com.runtimeterror.bcu_commu;
 
 public class IpAddress {
-    public static final String ip = "katep.iptime.org:8000";
+    public static final String ip = "192.168.25.42:8080";
 }
