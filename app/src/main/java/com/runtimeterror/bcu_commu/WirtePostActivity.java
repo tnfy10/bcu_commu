ffc1b50b840b12d365bdbfa4bc3e2c27a71e3c22
tnfy10 <tnfy10@gmail.com>
2020-12-03 23:55:17 +0900

    게시글 작성에서 제목, 내용을 입력하지 않아도 작성되던 문제 수정

--- app/src/main/java/com/runtimeterror/bcu_commu/WirtePostActivity.java
+++ app/src/main/java/com/runtimeterror/bcu_commu/WirtePostActivity.java
@@ -1,8 +1,10 @@
 package com.runtimeterror.bcu_commu;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -33,6 +35,11 @@ public class WirtePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wirte_post);
 
+        if(getIntent().getBooleanExtra("update", false)){
+            edtPostName.setText(getIntent().getStringExtra("title"));
+            edtPost.setText(getIntent().getStringExtra("content"));
+        }
+
         edtPostName = findViewById(R.id.edtPostName);
         edtPost = findViewById(R.id.edtPost);
 
@@ -47,138 +54,141 @@ public class WirtePostActivity extends AppCompatActivity {
         writeBtn = findViewById(R.id.writeBtn);
         writeBtn.setVisibility(View.VISIBLE);
 
-        if(getIntent().getBooleanExtra("update", false)){
-            updatePost();
-        }else{
-            writeBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    myHelper = new myDBHelper(getApplicationContext());
-                    sqlDB = myHelper.getReadableDatabase();
-                    Cursor cursor;
-                    cursor = sqlDB.rawQuery("SELECT * FROM userTBL;", null);
-                    cursor.moveToNext();
-
-                    String userId = cursor.getString(0);
-
-                    cursor.close();
-                    sqlDB.close();
-
-                    SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd");
-
-                    Calendar time = Calendar.getInstance();
-
-                    String postdate = format.format(time.getTime());
-
-                    Post post = new Post(edtPostName.getText().toString(), edtPost.getText().toString(), userId, postdate);
-
-                    Boolean freeBoard = getIntent().getBooleanExtra("freeBoard", false);
-                    Boolean meetBoard = getIntent().getBooleanExtra("meetBoard", false);
-                    Boolean subjBoard = getIntent().getBooleanExtra("subjBoard", false);
-
-                    if(freeBoard) {
-                        try {
-                            FreePost task = new FreePost();
-                            String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
-                            Boolean check = Boolean.parseBoolean(result);
-
-                            Log.d("check", check.toString());
-                            if(check) {
-                                Log.d("check", "게시글 작성에 성공하셨습니다.");
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), FreeBoardActivity.class));
-                            }
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if(meetBoard) {
-                        try {
-                            MeetPost task = new MeetPost();
-                            String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
-                            Boolean check = Boolean.parseBoolean(result);
-                            Log.d("checdk", check.toString());
-
-                            if(check) {
-
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), MeetBoardActivity.class));
-                            }
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if(subjBoard) {
-                        try {
-                            SubjectPost task = new SubjectPost();
-                            String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
-                            Boolean check = Boolean.parseBoolean(result);
-
-                            if(check) {
-                                Log.d("check", "게시글 작성에 성공하셨습니다.");
-                                finish();
-                                startActivity(new Intent(getApplicationContext(), SubjBoardActivity.class));
-                            }
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edtPostName.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "제목을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(edtPost.getText().toString().equals("")){
+                        Toast.makeText(getApplicationContext(), "내용을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        if(getIntent().getBooleanExtra("update", false)){
+                            updatePost();
+                        }else{
+                            writePost();
                         }
                     }
                 }
-            });
+            }
+        });
+    }
+
+    public void writePost(){
+        myHelper = new myDBHelper(getApplicationContext());
+        sqlDB = myHelper.getReadableDatabase();
+        Cursor cursor;
+        cursor = sqlDB.rawQuery("SELECT * FROM userTBL;", null);
+        cursor.moveToNext();
+
+        String userId = cursor.getString(0);
+
+        cursor.close();
+        sqlDB.close();
+
+        SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd");
+
+        Calendar time = Calendar.getInstance();
+
+        String postdate = format.format(time.getTime());
+
+        Post post = new Post(edtPostName.getText().toString(), edtPost.getText().toString(), userId, postdate);
+
+        Boolean freeBoard = getIntent().getBooleanExtra("freeBoard", false);
+        Boolean meetBoard = getIntent().getBooleanExtra("meetBoard", false);
+        Boolean subjBoard = getIntent().getBooleanExtra("subjBoard", false);
+
+        if(freeBoard) {
+            try {
+                FreePost task = new FreePost();
+                String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
+                Boolean check = Boolean.parseBoolean(result);
+
+                Log.d("check", check.toString());
+                if(check) {
+                    Log.d("check", "게시글 작성에 성공하셨습니다.");
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), FreeBoardActivity.class));
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(meetBoard) {
+            try {
+                MeetPost task = new MeetPost();
+                String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
+                Boolean check = Boolean.parseBoolean(result);
+                Log.d("checdk", check.toString());
+
+                if(check) {
+
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), MeetBoardActivity.class));
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(subjBoard) {
+            try {
+                SubjectPost task = new SubjectPost();
+                String result = task.execute(post.getTitle(), post.getContent(), post.getWriter(), post.getTime()).get();
+                Boolean check = Boolean.parseBoolean(result);
+
+                if (check) {
+                    Log.d("check", "게시글 작성에 성공하셨습니다.");
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), SubjBoardActivity.class));
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void updatePost(){
-        edtPostName.setText(getIntent().getStringExtra("title"));
-        edtPost.setText(getIntent().getStringExtra("content"));
+        String board = getIntent().getStringExtra("board");
+        String title = edtPostName.getText().toString();
+        String content = edtPost.getText().toString();
+        String postNum = getIntent().getStringExtra("postNum");
 
-        writeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String board = getIntent().getStringExtra("board");
-                String title = edtPostName.getText().toString();
-                String content = edtPost.getText().toString();
-                String postNum = getIntent().getStringExtra("postNum");
+        try {
+            UpdatePost task = new UpdatePost();
+            String result = task.execute(board, title, content, postNum).get();
+            Boolean check = Boolean.parseBoolean(result);
 
-                try {
-                    UpdatePost task = new UpdatePost();
-                    String result = task.execute(board, title, content, postNum).get();
-                    Boolean check = Boolean.parseBoolean(result);
-
-                    if(check) {
-                        Log.d("check", "게시글 수정에 성공하셨습니다.");
-                        finish();
-                        switch (board){
-                            case "free":
-                                startActivity(new Intent(getApplicationContext(), FreeBoardActivity.class));
-                                break;
-                            case "meet":
-                                startActivity(new Intent(getApplicationContext(), MeetBoardActivity.class));
-                                break;
-                            case "subj":
-                                startActivity(new Intent(getApplicationContext(), SubjBoardActivity.class));
-                                break;
-                            default:
-                                Toast.makeText(getApplicationContext(), "게시판 값 전달 에러", Toast.LENGTH_LONG).show();
-                                break;
-                        }
-
-                    }
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            if(check) {
+                Log.d("check", "게시글 수정에 성공하셨습니다.");
+                finish();
+                switch (board){
+                    case "free":
+                        startActivity(new Intent(getApplicationContext(), FreeBoardActivity.class));
+                        break;
+                    case "meet":
+                        startActivity(new Intent(getApplicationContext(), MeetBoardActivity.class));
+                        break;
+                    case "subj":
+                        startActivity(new Intent(getApplicationContext(), SubjBoardActivity.class));
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "게시판 값 전달 에러", Toast.LENGTH_LONG).show();
+                        break;
                 }
             }
-        });
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
